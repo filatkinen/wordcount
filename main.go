dbd6a17b36fb15a2c95102991b740bd5228682af
fenych <filatkinen@gmail.com>
2022-12-18 17:01:31 +0300

    word2

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println(len(os.Args) - 1)
+	var count int
+	for _, value := range os.Args[1:] {
+		strcount := strings.Split(value, " ")
+		for _, strvalue := range strcount {
+			if len(strvalue) != 0 {
+				count++
+			}
+		}
+	}
+	fmt.Println(count)
 }
